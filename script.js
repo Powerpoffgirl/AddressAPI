de2032da6fa86522d8c07e64a5151e1103643d8d
user.Powerpoffgirl <email.jyotisingh13@gmail.com>
2023-03-05 19:28:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const tbody = document.getElementById("tbody");
+const countryInfo = document.getElementById("countryInfo");
+
+// Fetch data from the API & I am storing data in response.
+let storeData = {}; //Using this variable to store the fetched data grobally.
+function fetchData() {
+  fetch(`https://disease.sh/v3/covid-19/countries`)
+    .then((responseData) => responseData.json())
+    .then((responseData) => {
+      storeData = responseData;
+      showData();
+    });
+}
+
+fetchData();
+
+function showData() {
+  storeData.map((data) => {
+    let row = document.createElement("tr");
+    row.innerHTML = `
+        <td>${data.active}</td>
+        <td>${data.country}</td>
+        <td id="countryInfo">${
+          "Id: " +
+          data.countryInfo._id +
+          " lat: " +
+          data.countryInfo.lat +
+          " long: " +
+          data.countryInfo.long +
+          " flag: " +
+          data.countryInfo.flag
+        }</td>
+        <td><button type="button" id="showMore" onclick="showMoreInfo()">+</button></td>
+    `;
+    tbody.append(row);
+  });
+}
+
+function showMoreInfo() {
+  //   let sb = document.getElementById("countryInfo")?.style.display = "block";
+  //   countryInfo.style.visibility = "visible";
+  console.log("show More");
+}
